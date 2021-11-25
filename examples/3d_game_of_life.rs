709bca6bec6413798c5ee5db617bb49ca0edffb0
Felix de Maneville <felix.maneville@gmail.com>
2021-11-25 16:56:26 +0100

    fixed 3D example

--- examples/3d_game_of_life.rs
+++ examples/3d_game_of_life.rs
@@ -1,4 +1,16 @@
-use bevy::prelude::*;
+use bevy::asset::Handle;
+use bevy::pbr2::{PbrBundle, StandardMaterial};
+use bevy::prelude::{
+    App, Assets, BuildChildren, Changed, Commands, DespawnRecursiveExt, Entity, GlobalTransform,
+    IVec3, Input, KeyCode, Query, Res, ResMut, Transform, Vec3, WindowDescriptor,
+};
+use bevy::render2::color::Color;
+use bevy::render2::view::Visibility;
+use bevy::render2::{
+    camera::PerspectiveCameraBundle,
+    mesh::{shape, Mesh},
+};
+use bevy::PipelinedDefaultPlugins;
 use bevy_life::{CellMap, ConwayCell4555State, GameOfLife3dPlugin, MooreCell3d, SimulationBatch};
 use rand::Rng;
 
@@ -12,12 +24,13 @@ fn main() {
             height: 800.,
             ..Default::default()
         })
-        .add_plugins(DefaultPlugins)
+        .add_plugins(PipelinedDefaultPlugins)
         .add_plugin(GameOfLife3dPlugin::default())
         .insert_resource(SimulationBatch::default())
         .add_startup_system(setup_camera)
         .add_startup_system(setup_map)
         .add_system(handle_reset_3d)
+        .add_system(color)
         .run();
 }
 
@@ -27,33 +40,49 @@ pub fn handle_reset_3d(
     map: Res<MapEntity>,
     mut cell_map: ResMut<CellMap<MooreCell3d>>,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     if keys.just_released(KeyCode::Space) {
         commands.entity(map.0).despawn_recursive();
         commands.remove_resource::<MapEntity>();
         cell_map.clear();
         println!("regenerating map");
-        spawn_map(&mut commands, &mut meshes);
+        let mesh = meshes.add(Mesh::from(shape::Cube::new(1.)));
+        let material = materials.add(StandardMaterial {
+            base_color: Color::WHITE,
+            unlit: true,
+            ..Default::default()
+        });
+        spawn_map(&mut commands, mesh, material);
     }
 }
 
 fn setup_camera(mut commands: Commands) {
     // Camera
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(50., 50., -50.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(50., 50., -100.).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
 }
 
-fn setup_map(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+fn setup_map(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh = meshes.add(Mesh::from(shape::Cube::new(1.)));
+    let material = materials.add(StandardMaterial {
+        base_color: Color::WHITE,
+        unlit: true,
+        ..Default::default()
+    });
     // map
-    spawn_map(&mut commands, &mut meshes);
+    spawn_map(&mut commands, mesh, material);
 }
 
-fn spawn_map(commands: &mut Commands, meshes: &mut Assets<Mesh>) {
-    let mesh = meshes.add(Mesh::from(shape::Cube::new(1.)));
+fn spawn_map(commands: &mut Commands, mesh: Handle<Mesh>, material: Handle<StandardMaterial>) {
     let mut rng = rand::thread_rng();
-    let map_size = 50;
+    let map_size = 60;
     let entity = commands
         .spawn()
         .insert(Transform::from_xyz(
@@ -71,6 +100,7 @@ fn spawn_map(commands: &mut Commands, meshes: &mut Assets<Mesh>) {
                             .spawn_bundle(PbrBundle {
                                 mesh: mesh.clone(),
                                 transform: Transform::from_xyz(x as f32, y as f32, z as f32),
+                                material: material.clone(),
                                 ..Default::default()
                             })
                             .insert(MooreCell3d::new(IVec3::new(x, y, z)))
@@ -82,4 +112,12 @@ fn spawn_map(commands: &mut Commands, meshes: &mut Assets<Mesh>) {
         .id();
     commands.insert_resource(MapEntity(entity));
     println!("map generated");
+}
+
+pub fn color(
+    mut query: Query<(&ConwayCell4555State, &mut Visibility), Changed<ConwayCell4555State>>,
+) {
+    for (state, mut visible) in query.iter_mut() {
+        visible.is_visible = state.0
+    }
 }
